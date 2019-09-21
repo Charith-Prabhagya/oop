2903673d44c75df02195d75506ea86b08a3bd466
charith prabhagya <cprabhagya@gmail.com>
2019-09-21 23:20:48 +0530

    teee

---
+++ testing.java
@@ -0,0 +1,17 @@
+/**
+ * testing
+ */
+public class testing {
+
+    public static void main(String[] args) {
+        System.out.println("hellooo");
+        char arr[] = { 'a', 'b', 'c' };
+        String a = new String(arr);
+        System.out.println(a);
+        String c = "asdfg";
+        String d = c.substring(1, 4);
+        System.out.println("helpiu".substring(2, 5));
+        System.out.println(d);
+
+    }
+}
